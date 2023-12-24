7cfb8dd6698cd5194dc48495ad9acf85765f6f5c
Gustavo Costa <guucoostaa@hotmail.com>
2023-12-24 01:08:57 -0300

    Moving pieces

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -5,19 +5,15 @@ import boardgame.Piece;
 
 public class ChessPiece extends Piece {
 
-		private Color color;
+	private Color color;
 
-		public ChessPiece(Board board, Color color) {
-			super(board);
-			this.color = color;
-		}
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-		public Color getColor() {
-			return color;
-		}
+	public Color getColor() {
+		return color;
+	}
 
-		
-		
-		
-	
 }
--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -5,10 +5,9 @@ import boardgame.Position;
 public class ChessPosition {
 	private char column;
 	private int row;
-	
-	
+
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column> 'h' || row < 1 || row > 8) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instanting ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
@@ -26,11 +25,11 @@ public class ChessPosition {
 	protected Position toPosition() {
 		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
-	
+
 	@Override
 	public String toString() {
 		return "" + column + row;
